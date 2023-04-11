5da5e089dc195f0e721e5903cf3ad7ce134c21f7
Moritz Vogt <moritz.vogt@raysono.com>
2023-04-11 10:44:12 +0200

    init Backend

---
+++ src/main/java/com/mamotec/energycontrolbackend/EnergycontrolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mamotec.energycontrolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergycontrolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergycontrolBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mamotec/energycontrolbackend/EnergycontrolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamotec.energycontrolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergycontrolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
