49ea4caa90b474e864467f23d7b7f35b3c8dcf01
Moritz Vogt <moritz.vogt@raysono.com>
2023-04-11 10:52:39 +0200

    setup: Change Name of the main class

--- src/main/java/com/mamotec/energycontrolbackend/EnergyControlBackendApplication.java
+++ src/main/java/com/mamotec/energycontrolbackend/EnergyControlBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EnergycontrolBackendApplication {
+public class EnergyControlBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EnergycontrolBackendApplication.class, args);
+		SpringApplication.run(EnergyControlBackendApplication.class, args);
 	}
 
 }
