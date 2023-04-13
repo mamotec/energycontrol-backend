d65a44595761769a31eec6b9b2457c185b88a8d0
Moritz Vogt <moritz.vogt@raysono.com>
2023-04-13 12:14:47 +0200

    setup: Add Admin user for mamotec

---
+++ src/main/resources/db/migration/V2__add_default_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO mamotec_user (id, first_name, last_name, email, username, password, role, created_at, updated_at)
+VALUES (default, 'MaMoTec', 'Admin', 'mamotec@outlook.de', 'mamotec', '$2a$10$FhT1lQ2Iny/Z6nuxCdhOJOppO/f11GalVwUg5BljPLsH7Dngi3YAy', 0, now(), NULL);
