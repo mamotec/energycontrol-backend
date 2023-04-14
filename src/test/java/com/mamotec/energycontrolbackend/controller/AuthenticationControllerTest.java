79d8bc82b25f097cf770116fe4308b33bc1858e8
Moritz Vogt <moritz.vogt@raysono.com>
2023-04-14 16:11:24 +0200

    test: Write Tests for AuthenticationController

--- src/test/java/com/mamotec/energycontrolbackend/controller/AuthenticationControllerTest.java
+++ src/test/java/com/mamotec/energycontrolbackend/controller/AuthenticationControllerTest.java
@@ -1,5 +1,6 @@
 package com.mamotec.energycontrolbackend.controller;
 
+import com.mamotec.energycontrolbackend.domain.auth.AuthenticationRequest;
 import com.mamotec.energycontrolbackend.domain.auth.AuthenticationResponse;
 import com.mamotec.energycontrolbackend.domain.auth.RegisterRequest;
 import com.mamotec.energycontrolbackend.service.AuthenticationService;
@@ -21,7 +22,7 @@ public class AuthenticationControllerTest extends BaseControllerTest {
 
     @Test
     public void shouldRegisterUser() throws Exception {
-
+        // given
         final RegisterRequest request = RegisterRequest.builder()
                 .firstName("MaMoTec")
                 .lastName("Admin")
@@ -36,6 +37,28 @@ public class AuthenticationControllerTest extends BaseControllerTest {
 
         when(authService.register(any(RegisterRequest.class))).thenReturn(response);
 
+        // then
+        mockMvc.perform(post("/auth/register").content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").value("2342424l2j4l23j32"));
+    }
+
+    @Test
+    public void shouldAuthenticateUser() throws Exception {
+        // given
+        final AuthenticationRequest request = AuthenticationRequest.builder()
+                .username("mvogt")
+                .password("Start1234*")
+                .build();
+
+        final AuthenticationResponse response = AuthenticationResponse.builder()
+                .token("2342424l2j4l23j32")
+                .build();
+
+        when(authService.register(any(RegisterRequest.class))).thenReturn(response);
+
+        // then
         mockMvc.perform(post("/auth/register").content(objectMapper.writeValueAsString(request))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
