76a73b80f34cd4ccce9060944bee1ea8dd888787
Moritz Vogt <moritz.vogt@vogges.de>
2023-08-24 14:54:53 +0200

    Modifiy Dockerfile

--- src/main/java/com/mamotec/energycontrolbackend/config/SecurityConfiguration.java
+++ src/main/java/com/mamotec/energycontrolbackend/config/SecurityConfiguration.java
@@ -52,6 +52,7 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setExposedHeaders(Arrays.asList("Authorization", "content-type"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type"));
