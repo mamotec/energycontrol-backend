31f3ab0a936b7b7096dd79352ce5aaf55c332e31
Moritz Vogt <moritz.vogt@vogges.de>
2023-08-29 23:13:45 +0200

    Modifiy Dockerfile

--- src/main/java/com/mamotec/energycontrolbackend/cron/ReadDeviceScheduler.java
+++ src/main/java/com/mamotec/energycontrolbackend/cron/ReadDeviceScheduler.java
@@ -1,5 +1,8 @@
 package com.mamotec.energycontrolbackend.cron;
 
+import com.ghgande.j2mod.modbus.facade.ModbusSerialMaster;
+import com.ghgande.j2mod.modbus.procimg.InputRegister;
+import com.ghgande.j2mod.modbus.util.SerialParameters;
 import com.mamotec.energycontrolbackend.client.NodeRedClient;
 import com.mamotec.energycontrolbackend.domain.device.Device;
 import com.mamotec.energycontrolbackend.domain.interfaceconfig.InterfaceConfig;
@@ -12,13 +15,6 @@ import com.mamotec.energycontrolbackend.service.interfaceconfig.InterfaceService
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.wimpi.modbus.Modbus;
-import net.wimpi.modbus.io.ModbusSerialTransaction;
-import net.wimpi.modbus.msg.ReadInputRegistersRequest;
-import net.wimpi.modbus.msg.ReadInputRegistersResponse;
-import net.wimpi.modbus.net.SerialConnection;
-import net.wimpi.modbus.procimg.InputRegister;
-import net.wimpi.modbus.util.SerialParameters;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -36,7 +32,7 @@ public class ReadDeviceScheduler {
     private final NodeRedClient nodeRedClient;
     private final DeviceDataService deviceDataService;
 
-    @Scheduled(cron = "*/59 * * * * *")
+    @Scheduled(cron = "*/10 * * * * *")
     @Transactional
     public void fetchDeviceData() {
         List<InterfaceConfig> configs = interfaceConfigService.findAll();
@@ -69,45 +65,33 @@ public class ReadDeviceScheduler {
     private void doFetchPerDevice(InterfaceConfig config, Device d, DeviceYaml deviceYaml, RegisterMapping mapping) throws IOException, InterruptedException {
         // Fetch data from node-red
         // String res = nodeRedClient.fetchDeviceData(deviceYaml, config, d.getUnitId(), mapping);
+        SerialParameters params = new SerialParameters();
+        params.setPortName("/dev/ttyS0");
+        params.setBaudRate(9600);
+        params.setDatabits(8);
+        params.setParity("None");
+        params.setStopbits(1);
+        params.setEncoding("rtu");
+
+        ModbusSerialMaster modbusClient = new ModbusSerialMaster(params); // Serieller Port
+
         try {
-            // Serielle Verbindungsparameter
-            SerialParameters serialParams = new SerialParameters();
-            serialParams.setPortName("/dev/ttyS0"); // Anpassen des seriellen Ports
-            serialParams.setBaudRate(9600);
-            serialParams.setDatabits(8);
-            serialParams.setParity("None");
-            serialParams.setStopbits(1);
-            serialParams.setEncoding(Modbus.SERIAL_ENCODING_RTU);
-            serialParams.setEcho(false);
 
-            // Serielle Verbindung herstellen
-            SerialConnection connection = new SerialConnection(serialParams);
-            connection.open();
+            modbusClient.connect();
+            int startAddress = 672; // Startadresse
+            int quantity = 2; // Anzahl von Registern
 
-            // Modbus-Anfrage erstellen
-            ReadInputRegistersRequest req = new ReadInputRegistersRequest(mapping.getRegister().get(0), mapping.getRegister().size()); // Startadresse: 672, Anzahl: 2
-            req.setUnitID(d.getUnitId()); // Unit-ID des Modbus-Slave-Geräts
-
-            // Modbus-Transaktion ausführen
-            ModbusSerialTransaction transaction = new ModbusSerialTransaction(connection);
-            transaction.setRequest(req);
-            transaction.execute();
-
-            // Antwort verarbeiten
-            ReadInputRegistersResponse res = (ReadInputRegistersResponse) transaction.getResponse();
-            if (res != null) {
-                InputRegister[] data = res.getRegisters().clone();
-                for (InputRegister value : data) {
-                    System.out.println("Value: " + value);
-                }
+            InputRegister[] inputRegisters = modbusClient.readInputRegisters(2, startAddress, quantity);
+            for (int i = 0; i < inputRegisters.length; i++) {
+                System.out.println("Register " + (startAddress + i) + ": " + inputRegisters[i]);
             }
 
-            // Serielle Verbindung schließen
-            connection.close();
-
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            modbusClient.disconnect();
         }
+
         // Save data to influxdb
         deviceDataService.writeDeviceData(d, "res", mapping);
     }
