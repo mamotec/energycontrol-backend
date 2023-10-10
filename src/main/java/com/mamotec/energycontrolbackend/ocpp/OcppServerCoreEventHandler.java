287b1a98f8e31e911f19c9f7910695e37793f4ad
Moritz Vogt <moritz.vogt@raysono.com>
2023-10-10 07:56:43 +0200

    Logging Charge Station

--- src/main/java/com/mamotec/energycontrolbackend/ocpp/OcppServerCoreEventHandler.java
+++ src/main/java/com/mamotec/energycontrolbackend/ocpp/OcppServerCoreEventHandler.java
@@ -5,12 +5,14 @@ import eu.chargetime.ocpp.feature.profile.ServerCoreEventHandler;
 import eu.chargetime.ocpp.model.Request;
 import eu.chargetime.ocpp.model.core.*;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.ZonedDateTime;
 import java.util.Set;
 import java.util.UUID;
 
 @AllArgsConstructor
+@Slf4j
 public class OcppServerCoreEventHandler implements ServerCoreEventHandler {
 
     private OcppSender sender;
@@ -21,43 +23,51 @@ public class OcppServerCoreEventHandler implements ServerCoreEventHandler {
 
     @Override
     public AuthorizeConfirmation handleAuthorizeRequest(UUID uuid, AuthorizeRequest authorizeRequest) {
+        log.info("AuthorizeRequest: {}", authorizeRequest);
         return null;
     }
 
     @Override
     public BootNotificationConfirmation handleBootNotificationRequest(UUID uuid, BootNotificationRequest bootNotificationRequest) {
+        log.info("BootNotificationRequest: {}", bootNotificationRequest);
         return null;
     }
 
     @Override
     public DataTransferConfirmation handleDataTransferRequest(UUID uuid, DataTransferRequest dataTransferRequest) {
+        log.info("DataTransferRequest: {}", dataTransferRequest);
         return null;
     }
 
     @Override
     public HeartbeatConfirmation handleHeartbeatRequest(UUID uuid, HeartbeatRequest heartbeatRequest) {
+        log.info("HeartbeatRequest: {}", heartbeatRequest);
         service.updateActiveStatus(uuid, heartbeatRequest.validate());
         return new HeartbeatConfirmation(ZonedDateTime.now());
     }
 
     @Override
     public MeterValuesConfirmation handleMeterValuesRequest(UUID uuid, MeterValuesRequest meterValuesRequest) {
+        log.info("MeterValuesRequest: {}", meterValuesRequest);
         return null;
     }
 
     @Override
     public StartTransactionConfirmation handleStartTransactionRequest(UUID uuid, StartTransactionRequest startTransactionRequest) {
+        log.info("StartTransactionRequest: {}", startTransactionRequest);
         return new StartTransactionConfirmation(new IdTagInfo(AuthorizationStatus.Accepted), 1);
     }
 
     @Override
     public StatusNotificationConfirmation handleStatusNotificationRequest(UUID uuid, StatusNotificationRequest statusNotificationRequest) {
+        log.info("StatusNotificationRequest: {}", statusNotificationRequest);
         service.updateChargePointStatus(uuid, statusNotificationRequest.getStatus());
         return new StatusNotificationConfirmation();
     }
 
     @Override
     public StopTransactionConfirmation handleStopTransactionRequest(UUID uuid, StopTransactionRequest stopTransactionRequest) {
+        log.info("StopTransactionRequest: {}", stopTransactionRequest);
         return null;
     }
 }
